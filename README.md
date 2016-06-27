62c114e2989c3cdf67e5efa4144464468880f46a
Rafal Radomski <rafal.radomski@hmhco.com>
2016-06-27 12:42:19 +0100

    init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # es6
 es6 Tutorial 
 https://www.youtube.com/playlist?list=PLYxzS__5yYQkvEC2Dr1W9CTODbFQ0GhBZ
+
+traceur --out js/script_compiled.js js/es6.js
---
+++ js/script_compiled.js
@@ -0,0 +1,7 @@
+$traceurRuntime.registerModule("es6.js", [], function() {
+  "use strict";
+  var __moduleName = "es6.js";
+  console.log('Hello World');
+  return {};
+});
+$traceurRuntime.getModule("es6.js" + '');
